77b9a53d8a0bb036aaf3966c6890990d48d32dcc
Julianakruger <128434084+Julianakruger@users.noreply.github.com>
2025-11-24 10:25:51 -0300

    Update message sent by senderClient

--- eventos/src/main/java/br/univille/Publisher.java
+++ eventos/src/main/java/br/univille/Publisher.java
@@ -31,7 +31,7 @@ public class Publisher {
 
         senderClient.sendMessage(
             new ServiceBusMessage(
-                "WALTER: nao acredito"));
+                "WALTER: Oii"));
 
     }
 }
